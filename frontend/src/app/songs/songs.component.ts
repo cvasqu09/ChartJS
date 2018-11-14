013f262c4d3dd19b202b7e63c369894a1c227827
Chris.V <cvasqu09@gmail.com>
2018-11-13 21:08:27 -0500

    Started adding to songs component

--- frontend/src/app/songs/songs.component.ts
+++ frontend/src/app/songs/songs.component.ts
@@ -1,8 +1,14 @@
-import { Component, OnInit, AfterViewInit, AfterContentInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  AfterContentInit
+} from '@angular/core';
 import { Playlist } from '../playlists/playlist.model';
 import { mergeMap, map, concatAll } from 'rxjs/operators';
 import Song from './song.model';
 import { PlaylistService } from '../services/abstract-playlist.service';
+import { SpotifyPlaylistService } from '../services/spotify-playlist.service';
 
 @Component({
   selector: 'app-songs',
@@ -11,12 +17,27 @@ import { PlaylistService } from '../services/abstract-playlist.service';
 })
 export class SongsComponent implements OnInit {
   isChartDataAvailable = false;
-  scatterData = [];
+  data = new Array<number>(10);
+
+  labels = [
+    '0-9',
+    '10-19',
+    '20-29',
+    '30-39',
+    '40-49',
+    '50-59',
+    '60-69',
+    '70-79',
+    '80-89',
+    '90-99'
+  ];
+
   songs: Song[] = [];
 
-  constructor(private playlistService: PlaylistService) { }
+  constructor(private spotifyPlaylistService: SpotifyPlaylistService) {}
 
   ngOnInit() {
+    this.isChartDataAvailable = false;
     // this.playlistService.getPlaylists().pipe(
     //   map((playlist: any) => {
     //     return new Playlist(playlist.name, playlist.tracks, playlist.images[0].url);
@@ -33,14 +54,35 @@ export class SongsComponent implements OnInit {
     //   console.log(JSON.stringify(song));
     // });
 
-    this.playlistService.getPlaylistTracks('hello').subscribe(song => {
+    this.spotifyPlaylistService.getStaticSongs().subscribe(song => {
       this.songs.push(song);
-
     });
   }
 
   onClick() {
-    debugger;
+    this.calculatePopularityData();
+    this.isChartDataAvailable = true;
   }
 
+  private calculatePopularityData() {
+    this.songs
+      .map(song => {
+        return this.getPopularityIndex(song.getPopularity());
+      })
+      .forEach((popularityIndex: number) => {
+        if (!this.data[popularityIndex]) {
+          this.data[popularityIndex] = 1;
+        } else {
+          this.data[popularityIndex]++;
+        }
+      });
+  }
+
+  private getPopularityIndex(songPopularity: number): number {
+    if (songPopularity < 100) {
+      return songPopularity % 10;
+    } else {
+      return 9;
+    }
+  }
 }
